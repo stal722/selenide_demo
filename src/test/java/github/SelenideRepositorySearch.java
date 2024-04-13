6ce6208fb7c5b1097a26e670bf7b34275e56b9b8
sergejtrifonov <stal722@mail.ru>
2024-04-13 23:15:27 +0300

    Add test shouldFindSelenideRepositoryAtTheTop

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,4 +1,18 @@
 package github;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SelenideRepositorySearch {
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+        open("https://github.com/");
+        $("[data-target='qbsearch-input.inputButton']").click();
+        $("[data-target='query-builder.input']").setValue("selenide").pressEnter();
+        $$("[data-hpc=true]").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide"));
+    }
 }
